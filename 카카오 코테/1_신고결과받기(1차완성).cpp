7467946b74cd84b6368967b98abc0193f808f921
MINGI JU <jmgman@outlook.kr>
2023-01-21 17:35:50 +0900

    설명절에도 코딩함ㅋㅋ

---
+++ 카카오 코테/1_신고결과받기(1차완성).cpp
@@ -0,0 +1,120 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 20
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+const char* id_list[] = { "muzi","frodo","apeach","neo" };
+const char* report[] = { "muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi" };
+int k = 0;
+
+size_t id_list_len = sizeof(id_list) / sizeof(id_list[0]);
+size_t report_len = sizeof(report) / sizeof(report[0]);
+
+int* number_k = (int*)calloc(id_list_len, sizeof(int));	// 제출할때는 solution에 넣으슈
+int ch_k = 0;
+
+
+
+
+bool str_compare(char a[]) {
+	for (int i = 0; i < id_list_len; i++) {
+		if (strcmp(id_list[i], a) == 0) {
+			ch_k = i;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// id_list_len은 배열 id_list의 길이입니다.
+// report_len은 배열 report의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* id_list[], size_t id_list_len,const char* report[], size_t report_len, int k) {
+	int* answer = (int*)calloc(id_list_len, sizeof(int));
+	for (int p = 0; p < report_len; p++) {
+		char same_t[30];
+		char* tmp_s[2] = { NULL, };
+		strcpy(same_t, report[p]);
+
+		char* ptr_s = strtok(same_t, " ");
+		tmp_s[0] = ptr_s;
+		ptr_s = strtok(NULL, " ");
+		tmp_s[1] = ptr_s;
+
+		str_compare(tmp_s[1]);
+		number_k[ch_k]++;
+
+		for (int q = p + 1; q < report_len; q++) {
+			if (strcmp(report[q], report[p]) == 0) {
+				number_k[ch_k]--;
+			}
+		}
+	
+	}
+
+
+	for (int i = 0; i <= id_list_len; i++) {
+		char mail[30];
+		char* tmp[2] = { NULL, };
+		strcpy(mail, report[i]);
+
+		char* ptr = strtok(mail, " ");
+		tmp[0] = ptr;
+		ptr = strtok(NULL, " ");
+		tmp[1] = ptr;
+	
+		if (str_compare(tmp[1]) && number_k[ch_k] >= k) {
+			str_compare(tmp[0]);
+			answer[ch_k]++;
+		}
+	}
+
+	return answer;
+}
+
+
+
+
+int main() {
+	int* get_mail = solution(id_list, id_list_len, report, report_len, 2);
+
+	printf("\n*   id_list[]   *\n");
+	printf(" [");
+	for (int i = 0; i < id_list_len; i++) {
+		printf(" %s ", id_list[i]);
+	}
+	printf("]");
+	printf("\n");
+
+
+	
+	printf("\n*   report[]   *\n");
+	printf(" [");
+	for (int j = 0; j < report_len; j++) {
+		printf(" %s, ", report[j]);
+	}
+	printf("]");
+	printf("\n");
+
+
+	printf("\n*   number_k[]   *\n");
+	printf(" [");
+	for (int i = 0; i < id_list_len; i++) {
+		printf(" %d ", number_k[i]);
+	}
+	printf("]");
+	printf("\n");
+
+
+	printf("\n*   answer[]   *\n");
+	printf(" [");
+	for (int j = 0; j < id_list_len; j++) {
+		printf(" %d ", get_mail[j]);
+	}
+	printf("]");
+
+}
--- 카카오 코테/1_신고결과받기(function).cpp
+++ 카카오 코테/1_신고결과받기(function).cpp
@@ -1,30 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 20
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-// id_list_len은 배열 id_list의 길이입니다.
-// report_len은 배열 report의 길이입니다.
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
-int* solution(const char* id_list[], size_t id_list_len, const char* report[], size_t report_len, int k) {
-	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-	int* answer = (int*)malloc(1);
-	return answer;
-}
-
-
 //{ "muzi","frodo","apeach","neo" };
 //{ "con","ryan" };
-const char* id_list[] = { "con","ryan" };
+const char *id_list[] = { "muzi","frodo","apeach","neo" };
 
 //2
 //3
-int k = 2;
+int k = 0;
 
 size_t id_list_len = sizeof(id_list) / sizeof(id_list[0]);
+size_t report_len = id_list_len * (id_list_len - 1);
+
+char** report = (char**)malloc(sizeof(char*) * report_len);
+int* number_k = (int*)calloc(id_list_len, sizeof(int));
+
 int ch_k = 0;
+int i = 0;
+
 
 
 bool str_compare(char command[]) {
@@ -37,27 +35,18 @@ bool str_compare(char command[]) {
 	return 0;
 }
 
-
-int main() {
-	size_t report_len = id_list_len * (id_list_len - 1);
-
-	//printf("id_list_len : %d\n", id_list_len);
-	//printf("report_len : %d\n", report_len);
-
-	char** report = (char**)malloc(sizeof(char*) * report_len);
+// id_list_len은 배열 id_list의 길이입니다.
+// report_len은 배열 report의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* id_list[], size_t id_list_len, char* report[], size_t report_len, int k) {
 	char command[BUFFER_SIZE];
 	char target[BUFFER_SIZE];
-
-	int* number_k = (int*)calloc(id_list_len,sizeof(int));
-	int i = 0;
 	
-
-	//신고자와 신고대상을 입력 받고 report[]에 저장, 각 신고자 별 신고 당한 횟수를 number_k[]에 저장
 	while (1) {
 		printf("$ ");
 		scanf("%s %s", command, target);
 
-		if (strcmp(command, "exit") == 0 || strcmp(target,"exit")==0) break;
+		if (strcmp(command, "exit") == 0 || strcmp(target, "exit") == 0) break;
 
 		if (str_compare(command) == 1) {
 			if (strcmp(command, target) != 0 && str_compare(target) == 1) {
@@ -86,11 +75,9 @@ int main() {
 		printf(" %s\n", report[j]);
 	}
 
-
-	// report[]에 동일한 데이터 (신고자와 신고대상이 같은 데이터)가 여러번 들어가는 경우
 	for (int p = 0; p < i; p++) {
-		for (int q = p+1; q < i; q++) {
-			if (strcmp(report[q], report[p])==0) {
+		for (int q = p + 1; q < i; q++) {
+			if (strcmp(report[q], report[p]) == 0) {
 				char same_t[30];
 				char* tmp_s[2];
 				strcpy(same_t, report[p]);
@@ -110,40 +97,46 @@ int main() {
 		}
 	}
 
-	
-	//report[]와 number_k[]를 사용하여 처리결과메일을 신고자마다 받을 횟수를 get_mail[]에 저장
-	int* get_mail;
-	get_mail = (int*)calloc(id_list_len, sizeof(int));
+	int* answer;
+	answer = (int*)calloc(id_list_len, sizeof(int));
 
 	for (int i = 0; i < id_list_len; i++) {
 		char rev_target[30];
-		char* tmp[2] = {NULL, };
+		char* tmp[2] = { NULL, };
 		int m = 0;
 		strcpy(rev_target, report[i]);
-		
+
 		//printf("%s\n", rev_target);
-		
+
 		char* ptr = strtok(rev_target, " ");
-		
+
 		while (ptr != NULL) {
-			
+
 			tmp[m] = ptr;
 			//printf("%s\n", tmp[m]);
 			m++;
 			ptr = strtok(NULL, " ");
-			
+
 		}
 
-		
+
 		if (str_compare(tmp[1]) && number_k[ch_k] >= k) {
 			str_compare(tmp[0]);
-			get_mail[ch_k]++;
+			answer[ch_k]++;
 		}
 	}
 
+	return answer;
+}
 
-	printf("\n*   get_mail[]   *\n");
-	printf("  [");
+
+
+
+int main() {
+	int* get_mail =	solution(id_list, id_list_len, report, report_len, 2);
+
+	printf("\n*   answer[]   *\n");
+	printf(" [");
 	for (int j = 0; j < id_list_len; j++) {
 		printf(" %d ", get_mail[j]);
 	}
