d98c71cd53029745c82bfc72e056ebbcf4242c85
King's meow <mingee641@gmail.com>
2023-09-10 14:15:37 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int A, B, C;
+
+	cin >> A >> B >> C;
+	
+	cout << (A + B) % C << endl;
+	cout << ((A % C) + (B % C)) % C << endl;
+	cout << (A * B) % C << endl;
+	cout << ((A % C) * (B % C)) % C << endl;
+
+
+
+}
