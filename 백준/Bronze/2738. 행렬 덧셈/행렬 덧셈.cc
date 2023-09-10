350ba9eab9344187a7a3905489eeb728b72d9a91
King's meow <mingee641@gmail.com>
2023-09-10 14:29:40 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int A[100][100];
+	int B[100][100];
+	int N, M = 0;
+	int k = 0;
+	scanf("%d %d", &N, &M);
+
+	
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &A[i][j]);
+		}
+	}
+	
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &B[i][j]);
+		}
+	}
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ", A[i][j] + B[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+}
