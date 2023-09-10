a2fde31aadb2328c82220c2db02900ed78f947c4
King's meow <mingee641@gmail.com>
2023-09-10 14:46:44 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (b >= c) {
+		cout << -1;
+		return 0;
+	}
+
+	cout << a / (c - b) + 1;
+	
+
+}
