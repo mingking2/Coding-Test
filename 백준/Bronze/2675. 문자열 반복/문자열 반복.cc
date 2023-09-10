3394ae782974cd092ed70deefa89de5f7db7b510
King's meow <mingee641@gmail.com>
2023-09-10 14:25:48 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int T;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int R;
+		string S;
+		cin >> R;
+		cin >> S;
+		for (int j = 0; j < S.length(); j++) {
+			for (int k = 0; k < R; k++) {
+				cout << S[j];
+			}
+		}
+		cout << endl;
+	}
+}
