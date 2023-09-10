180f95764e977e226cc2f80031d8ccce78728508
King's meow <mingee641@gmail.com>
2023-09-10 14:30:38 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int i = 0;
+	for (int sum = 2; sum <= n; i++) {
+		sum += 6 * i;
+	}
+	if (n == 1) i = 1;
+	cout << i;
+	return 0;
+}
