7140215182e81dfcc92c7f7ee146c43b020abf51
King's meow <mingee641@gmail.com>
2023-11-21 12:56:53 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=1;i<n;i++) {
+        for(int j=n;j>i;j--) {
+            cout << " ";
+        }
+        for(int k=0;k<2*i-1;k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+
+    for(int i=n;i>0;i--) {
+        for(int j=0;j<n-i;j++) {
+            cout << " ";
+        }
+
+        for(int k=2*i-1;k>0;k--) {
+            cout << "*";
+        }
+        
+        cout << endl;
+    }
+}
