78102d624d5d156f919054b2634ec11dcd2106db
King's meow <mingee641@gmail.com>
2023-12-12 16:40:26 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,32 +1,35 @@
 import sys
 
-
-def dfs(x, y, cnt, color):
-    matrix[x][y] = 0
+def dfs(y,x,cnt,color):
+    visited[y][x] = True
     for i in range(4):
-        nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n:
-            if matrix[nx][ny] == color:
-                cnt = dfs(nx, ny, cnt + 1, color)
+        nx = x + dx[i]
+        if 0<=ny<m and 0<=nx<n:
+            if not visited[ny][nx] and matrix[ny][nx] == color:
+                cnt = dfs(ny, nx, cnt + 1, color)
     return cnt
 
 
-# 두 정수를 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
+
+n, m = map(int, sys.stdin.readline().split())
 
 # 나머지 줄에서 문자열 행렬을 읽어들이기
-matrix = [list(input()) for _ in range(m)]
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+matrix = [sys.stdin.readline().strip() for _ in range(m)]
+visited = [[False for _ in range(n)] for _ in range(m)]
+
+
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 white = 0
 blue = 0
 for i in range(m):
     for j in range(n):
-        if matrix[i][j] == 'W':
-            white += (dfs(i, j, 1, 'W')) ** 2
-        elif matrix[i][j] == 'B':
+        if matrix[i][j] == 'W' and not visited[i][j]:
+            white += (dfs(i,j,1,'W')) ** 2
+        elif matrix[i][j] == 'B' and not visited[i][j]:
             blue += (dfs(i, j, 1, 'B')) ** 2
 
 print(white, blue)
