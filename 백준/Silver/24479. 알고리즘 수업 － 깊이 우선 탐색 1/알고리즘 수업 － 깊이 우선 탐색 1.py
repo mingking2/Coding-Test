4357cd8df50a34f7bc33a10fd2506070cffcc246
King's meow <mingee641@gmail.com>
2023-12-26 11:16:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 620 ms, Memory: 68780 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+def dfs(v):
+    global cnt
+    visited[v] = True
+    answer[v] = cnt
+    link[v].sort(reverse=False)
+    for i in link[v]:
+        if not visited[i]:
+            cnt +=1
+            dfs(i)
+
+
+n, m, r = map(int, sys.stdin.readline().split())
+link = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+answer = [0] * (n + 1)
+cnt = 1
+
+for _ in range(m):
+    a,b = map(int, sys.stdin.readline().split())
+    link[a].append(b)
+    link[b].append(a)
+
+dfs(r)
+
+for i in answer[1:]:
+    print(i)
